78d9a903494285450e5e837b172345844265b34b
Avik Nandi <aviknandi@yahoo.com>
2020-02-03 17:10:26 +0530

    added router and other pages

--- src/app/places/offers/offers.page.ts
+++ src/app/places/offers/offers.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PlacesService } from '../places.service';
+import { Places } from '../places.model';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-offers',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./offers.page.scss'],
 })
 export class OffersPage implements OnInit {
+  placelist: Places[];
 
-  constructor() { }
+  constructor(
+    private placesService: PlacesService,
+    private menuCrtl: MenuController ) { }
 
   ngOnInit() {
+    this.placelist = this.placesService.getAllPlaces();
   }
+
+  test(item) {
+    item.close();
+  }
+
+  // open() {
+  //   this.menuCrtl.open('m1');
+  // }
 
 }
